44818886116c70f34990aaf932879b112e1ca3f2
Isabelar07 <isabelarochas@outlook.com.br>
2021-02-25 14:25:58 -0300

    alterei a conexão de dados, para ser destruída quando o endpoint parar de ser executado

--- src/data/BaseDataBase.ts
+++ src/data/BaseDataBase.ts
@@ -7,14 +7,37 @@ dotenv.config();
 
 export class BaseDataBase {
 
-    protected static connection: Knex = knex({
-        client: "mysql",
-        connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            port: 3306,
-            database: process.env.DB_NAME
+    private static connection: Knex | null = null
+
+    protected tableName = {
+        users: "LabMusic_Users"
+    }
+
+    protected getConnection(): Knex {
+
+        if(!BaseDataBase.connection) {
+
+            BaseDataBase.connection = knex({
+                client: "mysql",
+                connection: {
+                    host: process.env.DB_HOST,
+                    user: process.env.DB_USER,
+                    password: process.env.DB_PASSWORD,
+                    port: 3306,
+                    database: process.env.DB_NAME
+            },    
+        })
         }
-    })
+
+        return BaseDataBase.connection;
+    }
+
+    public static async destroyConnection(): Promise<void> {
+
+        if(BaseDataBase.connection) {
+
+            await BaseDataBase.connection.destroy();
+            BaseDataBase.connection = null;
+        }
+    }
 }
--- src/data/UserBaseDataBase.ts
+++ src/data/UserBaseDataBase.ts
@@ -4,21 +4,19 @@ import { BaseDataBase } from "./BaseDataBase";
 
 export class UserDataBase extends BaseDataBase {
 
-    private static TABLE_NAME = "LabMusic_Users";
-
     public async insertUser(
         user: User
     ): Promise<void> {
         try {
 
-            await BaseDataBase.connection
+            await this.getConnection()
             .insert({
                 id: user.id,
                 name: user.name,
                 nickName: user.nickName,
                 email: user.email,
                 password: user.password
-            }).into(UserDataBase.TABLE_NAME)
+            }).into(this.tableName.users)
 
         } catch (error) {
             throw new CustomError(500, "An unexpected error ocurred")
