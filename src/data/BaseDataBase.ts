a085966d22aaa8d91b3f5b1a68936bd81d179060
Isabelar07 <isabelarochas@outlook.com.br>
2021-02-25 11:11:22 -0300

    conectei banco de dados

---
+++ src/data/BaseDataBase.ts
@@ -0,0 +1,17 @@
+import knex from "knex"
+import Knex from "knex";
+
+
+export class BaseDataBase {
+
+    protected connection: Knex = knex({
+        client: "mysql",
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: 3306,
+            database: process.env.DB_NAME
+        }
+    })
+}
